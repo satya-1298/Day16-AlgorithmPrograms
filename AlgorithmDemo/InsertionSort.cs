b1ed0e1f13c147224518cd9f4503aaca92c763d0
unknown <satya-1298>
2023-07-13 17:51:36 +0530

    [Satya]Add : performing insertion sort

---
+++ AlgorithmDemo/InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmDemo
+{
+    public class InsertionSort
+    {
+        public void Display()
+
+        {
+            string[] str = { "m", "u", "a","r" };
+            int s = str.Length;
+            for (int i = 0; i < s; i++)
+            {
+                string temp = "";
+                for (int j = i + 1; j < s; j++)
+                {
+
+                    if (str[i].CompareTo(str[j]) > 0)
+                    {
+                        temp = str[i];
+                        str[i] = str[j];
+                        str[j] = temp;
+                    }
+
+                }
+            }
+            Console.WriteLine("The sorted list is ");
+            foreach (string d in str)
+            {
+                Console.WriteLine(d);
+            }
+
+        }
+    }
+}
--- AlgorithmDemo/Program.cs
+++ AlgorithmDemo/Program.cs
@@ -13,13 +13,22 @@ namespace AlgorithmDemo
             while(true) 
             {
                 Console.WriteLine("Choose a Option");
+                Console.WriteLine("****************************");
                 Console.WriteLine("1-Two Strings are Equal or not");
+                Console.WriteLine("2-InsertionSort");
+                Console.WriteLine("****************************");
                 int option=Convert.ToInt32(Console.ReadLine());
                 switch(option) 
                 {
                     case 1:
                         AlgorithmProg algorithmProg = new AlgorithmProg();
                         algorithmProg.Word();
+                        Console.WriteLine("****************************");
+                        break;
+                    case 2:
+                        InsertionSort sort = new InsertionSort();
+                        sort.Display();
+                        Console.WriteLine("****************************");
                         break;
                 }
             }
