881419e213dbaba495336201370ece99cdadf774
unknown <satya-1298>
2023-07-13 17:34:17 +0530

    [Satya]Add : Searching a word from the given file

---
+++ AlgorithmDemo/AlgorithmProg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mime;
+
+namespace AlgorithmDemo
+{
+    public class AlgorithmProg
+    {
+        public void Word()
+        {
+            Console.WriteLine("Enter a string");
+            string text = Console.ReadLine();
+            string path= "C:\\Users\\PC\\Desktop\\RFP288-Assignment\\Day16-AlgorithmPrograms\\AlgorithmDemo\\File.txt";
+            string read =File.ReadAllText(path);
+            //string[] word =read.Split(" ");
+            string[] word = read.Split(' ');
+            foreach (string str in word)
+            {
+                Console.WriteLine(str);
+                
+
+            }
+            foreach (string str in word)
+            {
+
+                if (text == str)
+                {
+                    Console.WriteLine("Word found");
+                }
+                else { Console.WriteLine("Word not found"); }
+            }
+
+
+        }
+    }
+}
---
+++ AlgorithmDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) 
+            {
+                Console.WriteLine("Choose a Option");
+                Console.WriteLine("1-Two Strings are Equal or not");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option) 
+                {
+                    case 1:
+                        AlgorithmProg algorithmProg = new AlgorithmProg();
+                        algorithmProg.Word();
+                        break;
+                }
+            }
+        }
+    }
+}
